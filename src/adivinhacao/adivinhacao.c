20a32ae592ff8575627288c480467d56ca57bfa5
joelvitorniino <joelprofessor13@gmail.com>
2022-09-09 20:49:37 -0300

    conditional

--- src/adivinhacao/adivinhacao.c
+++ src/adivinhacao/adivinhacao.c
@@ -11,30 +11,41 @@ int main()
     int numerosecreto = 42;
 
     int chute;
+    int tentativas = 1;
 
-    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+    while(1) {
+        printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
-        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
 
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
 
+        if (chute < 0) {
+            printf("Você não pode chutar números negativos!\n");
+            continue;
+        }
+
         int acertou = (chute == numerosecreto);
         int maior = (chute > numerosecreto);
 
-        if (acertou) {
+        if(acertou) {
             printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador!\n");
+            printf("Jogue de novo, você um bom jogador!\n");
 
             break;
         }
+
         else if (maior) {
             printf("Seu chute foi maior que o número secreto\n");
         }
+        
         else {
             printf("Seu chute foi menor que o número secreto\n");
         }
+
+        tentativas++;
     }
 
     printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas\n", tentativas);
 }
