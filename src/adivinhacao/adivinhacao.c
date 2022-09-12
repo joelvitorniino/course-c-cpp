df2244b765aa5300c6cc57891342cdb32aa35537
joelvitorniino <joelprofessor13@gmail.com>
2022-09-11 21:26:07 -0300

    switch statement

--- src/adivinhacao/adivinhacao.c
+++ src/adivinhacao/adivinhacao.c
@@ -20,7 +20,31 @@ int main()
     int tentativas = 1;
     double pontos = 1000;
 
-    while(1) {
+    int acertou = 0;
+
+    int nivel;
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int numerodetentativas;
+
+    switch(nivel) {
+        case 1:
+            numerodetentativas = 20;
+            break;
+            
+        case 2:
+            numerodetentativas = 15;
+            break;
+            
+        default:
+            numerodetentativas = 6;
+            break;
+    }
+
+    for(int i = 1; i <= numerodetentativas; i++){
         printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
 
@@ -32,13 +56,10 @@ int main()
             continue;
         }
 
-        int acertou = (chute == numerosecreto);
+        acertou = (chute == numerosecreto);
         int maior = (chute > numerosecreto);
 
         if(acertou) {
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você um bom jogador!\n");
-
             break;
         }
 
@@ -58,6 +79,13 @@ int main()
     }
 
     printf("Fim de jogo!\n");
-    printf("Você acertou em %d tentativas\n", tentativas);
-    printf("Total de pontos: %.1f\n", pontos);
+
+    if(acertou) {
+        printf("Você ganhou!\n");
+        printf("Você acertou em %d tentativas\n", tentativas);
+        printf("Total de pontos: %.1f\n", pontos);
+    } else {
+        printf("Você perdeu! Tente de novo!\n");
+    };
+
 }
