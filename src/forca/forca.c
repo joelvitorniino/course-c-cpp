1398c10a3ebdc2fac8b6f9947e0717cae7e75323
joelvitorniino <joelprofessor13@gmail.com>
2022-09-12 22:08:44 -0300

    manipulate array

--- src/forca/forca.c
+++ src/forca/forca.c
@@ -2,5 +2,12 @@
 
 int main()
 {
-    return 0;
+   int notas[10];
+
+   notas[0] = 10;
+   notas[2] = 9;
+   notas[3] = 8;
+   notas[9] = 4;
+
+   printf("notas %d %d %d", notas[0], notas[2], notas[9]);
 }
