3209b994f4f1cd44290fabaecce0281e95734533
joelvitorniino <joelprofessor13@gmail.com>
2022-09-16 22:36:12 -0300

    for[

--- src/forca/forca.c
+++ src/forca/forca.c
@@ -10,15 +10,33 @@ int main()
    int acertou = 0;
    int enforcou = 0;
 
-   do {
-      char chute;
-      scanf("%c", &chute);
+   char chutes[26];
+   int tentativas = 0;
 
+   do {
       for(int i = 0; i < strlen(palavrasecreta); i++) {
-         if(palavrasecreta[i] == chute) {
-            printf("A posição %d tem essa letra!\n", i);
+         int achou = 0;
+
+         for(int j = 0; j < tentativas; j++) {
+            if(chutes[j] == palavrasecreta[i]) {
+               achou = 1;
+               break;
+            }
+         }
+
+         if(achou) {
+            printf("%c", palavrasecreta[i]);  
+         } else {
+            printf("_ ");
          }
       }
+      printf("\n");
+
+      char chute;
+      scanf(" %c", &chute);
+
+      chutes[tentativas] = chute;
+      tentativas++;
 
    } while(!acertou && !enforcou);
 }
