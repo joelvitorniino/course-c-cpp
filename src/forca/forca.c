60d6b1019b48da2b41fd9c46f6a706e822051c39
joelvitorniino <joelprofessor13@gmail.com>
2022-09-22 22:05:42 -0300

    implement not enforcou

--- src/forca/forca.c
+++ src/forca/forca.c
@@ -61,10 +61,32 @@ void escolhepalavra()
    sprintf(palavrasecreta, "MELANCIA");
 }
 
+int enforcou() 
+{
+   int erros = 0;
+
+   for(int i = 0; i < tentativas; i++)
+   {
+      int existe = 0;
+
+      for(int j = 0; j < strlen(palavrasecreta); j++) 
+      {
+         if(chutes[i] == palavrasecreta[j]) 
+         {
+            existe = 1;
+            break;
+         }
+      }
+
+      if(!existe) erros++;
+   }
+
+   return erros >= 5;
+}
+
 int main()
 {
    int acertou = 0;
-   int enforcou = 0;
 
    escolhepalavra();
    abertura();
@@ -73,5 +95,5 @@ int main()
    {
       desenhaforca();
       chuta();
-   } while(!acertou && !enforcou);
+   } while(!acertou && !enforcou());
 }
